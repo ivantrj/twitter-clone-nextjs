611b70c27bab74998aee116df3eb7aeea6a57b0c
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-08-07 13:33:53 +0200

    setup tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
-      Hello
+    <div className='text-3xl text-sky-500'>
+      Hello World!
     </div>
   );
 }
